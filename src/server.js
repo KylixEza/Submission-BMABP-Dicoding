6554371b6e04aec036c7d3f14e7d2ec3fb847770
Kylix <58837451+KylixEza@users.noreply.github.com>
2021-06-01 23:18:20 +0700

    update

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const routes = require('./routes');
 const init = async() => {
     const server = Hapi.server({
         port: 5000,
-        host: process.env.NODE_ENV === 'development' ? 'localhost': 'https://bookshelf-kylix.herokuapp.com',
+        host: process.env.PORT,
         routes: {
             cors: {
                 origin: ['*']
